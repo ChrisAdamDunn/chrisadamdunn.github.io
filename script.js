29327f2b38b980cb6180831cd19c4f5f3c1d9ecf
ChrisAdamDunn <89852924+ChrisAdamDunn@users.noreply.github.com>
2025-11-24 22:27:34 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-alert("Hi");
+"use strict";
+
+window.onload = init;
+
+let FPS;
+const FPSTarget = 60;
+let lastFPSTime;
+let FPSTargetInterval;
+
+
+
+function init(){
+	FPS = 0;
+	FPSTargetInterval = 1000 / FPSTarget;
+	lastFPSTime = window.performance.now();
+	
+	window.setTimeout(tick, 0);
+}
+
+function tick(){
+	//FPS counter
+	let thisFrameTime = window.performance.now();
+	if(thisFrameTime > lastFPSTime + 1000){
+		console.log("FPS ",FPS);
+document.getElementById("tl").innerHTML=FPS;
+		FPS = 0;
+		lastFPSTime = thisFrameTime;
+	}
+	FPS++;
+	
+	//logic
+	console.log("beat");
+	
+	draw();
+	
+	//Setup next tick
+	window.setTimeout(tick,((thisFrameTime + FPSTargetInterval) - window.performance.now()))
+}
+
+function draw(){
+
+}
