be9ef948d5a4da93aafdd22238d00a0a1b5d1bde
ChrisAdamDunn <89852924+ChrisAdamDunn@users.noreply.github.com>
2025-11-25 00:16:32 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -3,16 +3,25 @@
 window.onload = init;
 
 let FPS;
+let FPSDisplay;
 const FPSTarget = 60;
 let lastFPSTime;
 let FPSTargetInterval;
 
-
+let canvas;
+let display;
 
 function init(){
 	FPS = 0;
+	FPSDisplay = 0;
 	FPSTargetInterval = 1000 / FPSTarget;
 	lastFPSTime = window.performance.now();
+	
+	canvas = document.getElementById("canvas");
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	display = canvas.getContext("2d");
+	display.font = "30px Arial";
 	
 	window.setTimeout(tick, 0);
 }
@@ -22,7 +31,7 @@ function tick(){
 	let thisFrameTime = window.performance.now();
 	if(thisFrameTime > lastFPSTime + 1000){
 		console.log("FPS ",FPS);
-document.getElementById("tl").innerHTML=FPS;
+		FPSDisplay = FPS;
 		FPS = 0;
 		lastFPSTime = thisFrameTime;
 	}
@@ -30,13 +39,13 @@ document.getElementById("tl").innerHTML=FPS;
 	
 	//logic
 	console.log("beat");
+	console.log(canvas.width,canvas.height);
 	
-	draw();
+	display.fillStyle="red";
+	display.fillRect(0, 0, 999999, 999999);
+	display.fillStyle="white";
+	display.fillText(FPSDisplay,0,30);
 	
 	//Setup next tick
 	window.setTimeout(tick,((thisFrameTime + FPSTargetInterval) - window.performance.now()))
-}
-
-function draw(){
-
 }
